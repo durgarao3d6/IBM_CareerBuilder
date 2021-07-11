394be777e3999c7a6c93fdbaaa6af81091ea1f1f
durgarao3d6 <durgarao3d6@gmail.com>
2021-07-11 19:39:29 +0530

    'gitcommit'

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,15 @@
 function login(){
-    sessionStorage.set('User',document.getElementById('username').value);
-    alert('Successfully logged in');
+    sessionStorage.setItem('User',document.getElementById('username').value);
+    return true;
+}
+
+if(sessionStorage.User){
+    var User = sessionStorage.User;
+    document.getElementById('login').style.display='none';
+    document.getElementById('user').style.display='visible';
+    document.getElementById('user').innerHTML='<p class="userStyle" onClick="logout()">'+'Logout'+'</p>';
+}
+
+function logout(){
+    sessionStorage.User='';
 }
